0549633f00fe97adc4c050978f00d7ac7e79f9aa
davidburlaka <62345662+davidburlaka@users.noreply.github.com>
2021-02-17 11:47:58 +0200

    First commit

--- app/src/main/java/com/example/lab1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/lab1/ui/home/HomeFragment.java
@@ -16,12 +16,8 @@ import com.example.lab1.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         return root;
